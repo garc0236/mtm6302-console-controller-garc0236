374c7b8d4dd0504b63eba1e945f7a6ce1610a40c
Camilo Garcia <garc0236@algonquinlive.com>
2023-02-16 11:14:10 -0500

    makes points 1-3

---
+++ script.js
@@ -0,0 +1,29 @@
+const title = document.getElementById('headOne')
+
+
+function setTitle() {
+    title.textContent = "New Text"
+
+}
+
+
+
+const paragraph = document.querySelector('.paraOne')
+
+function setDescription() {
+    paragraph.textContent = "This is a new description of Lorem Ipsum Paragraph"
+
+}
+
+const background = document.getElementById('bbody')
+
+function setBackgroundColor() {
+    background.style.background = 'grey'
+}
+
+
+const text = [ title, paragraph]
+
+function setFontColor() {
+    text.style.color = blue;
+}
